e127cfb5bb206ecb9d429bcca95ae10755cd4e88
avaibhav54 <agarwalvaibhav041@gmail.com>
2025-08-14 00:11:44 +0530

    added remotion in the package

--- tauri_app/vite.config.ts
+++ tauri_app/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig(async () => ({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@remotion-shared': path.resolve(__dirname, '../../remotion_starter_template/src'),
+      '@remotion-shared': path.resolve(__dirname, '../packages/remotion_starter_template/src'),
     },
   },
 
